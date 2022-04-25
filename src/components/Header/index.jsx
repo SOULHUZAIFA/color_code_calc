c08edc2f1127bda51312bd51f354a448ff86cfca
Huzaifa Mehdi <huzaifamehdi@Huzaifas-MacBook-Pro.local>
2022-04-25 13:09:10 -0500

    First Commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './style.css';
+
+const Header = () => (
+  <header className="header">
+    4-Band Resistor Color Code Calculator
+  </header>
+);
+
+export default Header;
